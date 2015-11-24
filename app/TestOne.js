647fc6a1f1022590c30342b1730172de715acc36
mosutw <luke1974@gmail.com>
2015-11-25 06:20:55 +0800

    first commit

---
+++ app/TestOne.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+
+var TestOne = React.createClass({
+    render() {
+        return (
+            /* jshint ignore: start*/
+            <div>Hello I am TestOne Component</div>
+            /* jshint ignore: end*/
+        );
+    }
+});
+
+module.exports = TestOne;
